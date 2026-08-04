32154e69cea59eaaf10955726dbbdd8d64b9deb5
Aslam Maududy Atturraby <aslammaududy@users.noreply.github.com>
2026-08-04 15:41:42 +0700

    Revert "wip(appointment): change log from console to ILogger and fix bug when returning the queue number"
    This reverts commit c32390640a75d5fed3c1f9a2b79fe6b26d575739.

--- Services/QueueNumberGenerator.cs
+++ Services/QueueNumberGenerator.cs
@@ -3,35 +3,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HealthDeskAPI.Services;
 
-public class QueueNumberGenerator(HealthDeskApiContext context, ILogger<QueueNumberGenerator>  logger)
+public class QueueNumberGenerator
 {
-    private static readonly SemaphoreSlim Lock = new(1, 1);
+    private readonly HealthDeskApiContext _context;
+    private static readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
+    
+    public QueueNumberGenerator(HealthDeskApiContext context)
+    {
+        _context = context;
+    }
 
     public async Task<int> Generate(Appointment appointment)
     {
         var today = DateOnly.FromDateTime(DateTime.Now);
 
-        await Lock.WaitAsync();
+        await _lock.WaitAsync();
         try
         {
-            var counter = await context.Appointments.FirstOrDefaultAsync(q => q.AppointmentDate == today && q.DoctorId == appointment.DoctorId);
+            var counter = await _context.Appointments.FirstOrDefaultAsync(q => q.AppointmentDate == today && q.DoctorId == appointment.DoctorId);
 
             if (counter == null)
             {
                 return 1;
             }
 
-            counter.QueueNumber++;
-            return counter.QueueNumber;
+            return counter.QueueNumber++;
         }
         catch (Exception e)
         {
-            logger.LogError(e, e.Message);
+            Console.WriteLine(e);
             throw;
         }
         finally
         {
-            Lock.Release();
+            _lock.Release();
         }
     }
 }
